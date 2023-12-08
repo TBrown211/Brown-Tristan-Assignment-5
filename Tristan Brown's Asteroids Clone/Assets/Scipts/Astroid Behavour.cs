0c84eb294a2d457daa109325b5a9e0833968645b
TBrown211 <150560857+TBrown211@users.noreply.github.com>
2023-12-08 12:45:19 -0500

    Added basic asteroid script

--- Tristan Brown's Asteroids Clone/Assets/Scipts/Astroid Behavour.cs
+++ Tristan Brown's Asteroids Clone/Assets/Scipts/Astroid Behavour.cs
@@ -6,7 +6,9 @@ public class AstroidBehavour : MonoBehaviour
 {
     void Start()
     {
-        
+        float astroidX = Random.Range(-8.11f, 8.11f);
+        float astroidY = Random.Range(-4.23f, 4.23f);
+        transform.position = new Vector3(astroidX, astroidY, 0);
     }
 
     void Update()
