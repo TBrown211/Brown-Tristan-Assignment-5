c7fbb9684e4ed28a2356ab671938f7401f04c586
TBrown211 <150560857+TBrown211@users.noreply.github.com>
2023-12-12 14:28:02 -0500

    Added Asteroid Movement
    Asteroid can now move in a random direction

--- Tristan Brown's Asteroids Clone/Assets/Scipts/Astroid Behavour.cs
+++ Tristan Brown's Asteroids Clone/Assets/Scipts/Astroid Behavour.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class AstroidBehavour : MonoBehaviour
 {
+    public Vector2 velocity;
+    public Rigidbody2D asteroidBody;
+
     void Start()
     {
         float astroidX = Random.Range(-8.11f, 8.11f);
         float astroidY = Random.Range(-4.23f, 4.23f);
+        velocity.x = Random.Range(-3f, 3f);
+        velocity.y = Random.Range(-3f, 3f);
+        velocity = new Vector2 (velocity.x, velocity.y);
+        asteroidBody = GetComponent<Rigidbody2D> ();
         transform.position = new Vector3(astroidX, astroidY, 0);
+
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.Q))
-            SplitAsteroid();
+      asteroidBody.MovePosition(asteroidBody.position + velocity * Time.fixedDeltaTime);
     }
 
     void SplitAsteroid()
