d2852812ea7de6f8771833b6d6bb93a8edab472a
TBrown211 <150560857+TBrown211@users.noreply.github.com>
2023-12-13 13:50:56 -0500

    Attempting to use AddForce instead of MovePostion

--- Tristan Brown's Asteroids Clone/Assets/Scipts/Astroid Behavour.cs
+++ Tristan Brown's Asteroids Clone/Assets/Scipts/Astroid Behavour.cs
@@ -4,24 +4,26 @@ using UnityEngine;
 
 public class AstroidBehavour : MonoBehaviour
 {
-    public Vector2 velocity;
+    public float force = 3f;
     public Rigidbody2D asteroidBody;
 
     void Start()
     {
         float astroidX = Random.Range(-8.11f, 8.11f);
         float astroidY = Random.Range(-4.23f, 4.23f);
-        velocity.x = Random.Range(-3f, 3f);
-        velocity.y = Random.Range(-3f, 3f);
-        velocity = new Vector2 (velocity.x, velocity.y);
-        asteroidBody = GetComponent<Rigidbody2D> ();
+        //velocity.x = Random.Range(-3f, 3f);
+        //velocity.y = Random.Range(-3f, 3f);
+        //velocity = new Vector2 (velocity.x, velocity.y);
+        //asteroidBody = GetComponent<Rigidbody2D> ();
         transform.position = new Vector3(astroidX, astroidY, 0);
+        Vector3 giveAsteroidForce = Random.onUnitSphere * force;
+        asteroidBody.AddForce(giveAsteroidForce, ForceMode2D.Impulse);
 
     }
 
     void FixedUpdate()
     {
-      asteroidBody.MovePosition(asteroidBody.position + velocity * Time.fixedDeltaTime);
+     // asteroidBody.MovePosition(asteroidBody.position + velocity * Time.fixedDeltaTime);
     }
 
     void SplitAsteroid()
