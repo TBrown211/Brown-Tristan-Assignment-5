7b472d4b011943f1fa37e6f3d678eeda264ba9c5
TBrown211 <150560857+TBrown211@users.noreply.github.com>
2023-12-14 14:39:15 -0500

    Added Splitting function to Asteroids
    Still need to figure out how to have the asteroids stop splitting after a certain number of times

--- Tristan Brown's Asteroids Clone/Assets/Scipts/Astroid Behavour.cs
+++ Tristan Brown's Asteroids Clone/Assets/Scipts/Astroid Behavour.cs
@@ -6,6 +6,7 @@ public class AstroidBehavour : MonoBehaviour
 {
     public float force = 3f;
     public Rigidbody2D asteroidBody;
+    private int asteroidSplitCount = 0;
 
     void Start() //Gives the asteroid a random  postion on screen and then "pushes" it into a random direction.
     {
@@ -17,13 +18,39 @@ public class AstroidBehavour : MonoBehaviour
         transform.position = new Vector3(astroidX, astroidY, 0);
     }
 
-    void FixedUpdate()
+    void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.Space))
+            SplitAsteroid();
     }
 
     void SplitAsteroid()
     {
+        int maxAsteroidSplitCount = 3;
+
+        if (asteroidSplitCount < maxAsteroidSplitCount)
+        {
+            GameObject split1 = Instantiate(gameObject);
+            GameObject split2 = Instantiate(gameObject);
+            split1.transform.localScale = split1.transform.localScale / 2;
+            split2.transform.localScale = split2.transform.localScale / 2;
+            AstroidBehavour astroid1 = split1.GetComponent<AstroidBehavour>();
+            astroid1.asteroidSplitCount++;
+            AstroidBehavour astroid2 = split1.GetComponent<AstroidBehavour>();
+            astroid2.asteroidSplitCount++;
+            Destroy(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        //Destroy(gameObject); 
+
+        
+        
+        //split1.transform.position = transform.position;
+
 
     }
 }
